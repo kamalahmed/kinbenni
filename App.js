2cd797cd0c8a4738eaaf3c31ca67d3333eae947f
Kamal Ahmed <kamalacca@gmail.com>
2020-07-01 01:02:33 +0600

    added some dummy component with image

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, Image, SafeAreaView, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text
+        style={{
+          fontSize: 30,
+          marginBottom: 10,
+        }}
+      >
+        Daily Dose of Fun
+      </Text>
+
+      <Image
+        source={require("./assets/cat.jpg")}
+        style={{
+          width: 200,
+          height: 200,
+          borderRadius: 8,
+        }}
+      />
+
+      <Text
+        numberOfLines={2}
+        style={{
+          padding: 30,
+        }}
+      >
+        আসসালামু আলাইকুম বন্ধুরা. আমি তো মোবাইলের এপ বানানো শিখতেছি। তোমারা একটু
+        দোয়া করবা তো??
+      </Text>
+      <Button title={"It was fun"} onPress={() => alert("hank you")} />
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
